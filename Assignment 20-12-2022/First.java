8fb589f673b2968e9499050893aedc5a56e1cf18
Madhurima Burman <120620887+madhurimabur00@users.noreply.github.com>
2022-12-28 20:52:01 +0530

    Create First.java

---
+++ Assignment 20-12-2022/First.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class First {
+	public static void main(String[] args) {
+		int i;
+		for (i=0; i<=5; i++)
+		{
+			System.out.println("hello");
+		}
+		System.out.println();
+		for(i=5;i>0;i--)
+			{
+			System.out.println("world");
+			}
+		}
+}

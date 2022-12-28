272d8ff34102f294a53f338dbf089ce6f978fec7
Madhurima Burman <120620887+madhurimabur00@users.noreply.github.com>
2022-12-28 21:15:58 +0530

    Create  Circle .java

---
+++ Assignment 26-12-2022/Circle .java
@@ -0,0 +1,36 @@
+package abstraction;
+
+import java.util.Scanner;
+
+public class Circle {
+	final double pi=3.14;
+	
+	
+	public double CalculateArea(double pi,int radious)
+	{
+		double area;
+		area=pi* radious* radious;
+				return area;
+	}
+	public double CalculateCircum(double pi,int radious) {
+		double cir;
+		cir=2* pi* radious;
+		return cir;
+		}
+	
+
+	public static void main(String[] args) {
+		Circle circle=new Circle();
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enetr the radious:");
+			int r=sc.nextInt();
+			double result1=circle.CalculateArea(circle.pi,r);
+			System.out.println("Area of circle is:"+result1);
+			
+			double result2=circle.CalculateCircum(circle.pi,r);
+			System.out.println("Area of circle is:"+result2);
+		}
+
+	}
+
+}
